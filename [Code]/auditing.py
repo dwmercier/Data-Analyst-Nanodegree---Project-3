4743ab2cfaf21a0303afdaa7aabb19cebe700c8e
Daniel Mercier <dmercier@gmail.com>
2015-12-16 14:19:19 -0500

    - Add function to output street names in dictionary organized by street type - Rename and clean numerous function names

--- [Code]/auditing.py
+++ [Code]/auditing.py
@@ -2,23 +2,14 @@
 # -*- coding: utf-8 -*-
 # TODO: add file description
 # TODO: combine overview with auditing?
-"""
-Your task in this exercise has two steps:
 
-- audit the filename and change the variable 'mapping' to reflect the changes needed to fix 
-    the unexpected street types to the appropriate ones in the expected list.
-    You have to add mappings only for the actual problems you find in this filename,
-    not a generalized solution, since that may and will depend on the particular area you are auditing.
-- write the update_name function, to actually fix the street name.
-    The function takes a string with street name as an argument and should return the fixed name
-    We have provided a simple test so that you see what exactly is expected
-"""
 import xml.etree.cElementTree as ET
 from collections import defaultdict
 import re
 import pprint
 
-street_types = defaultdict(int)
+street_types_frequency = defaultdict(int)
+street_types_set = defaultdict(set)
 
 ### Regex filters
 street_type_re_english = re.compile(r'\S+\.?$', re.IGNORECASE)
@@ -39,64 +30,51 @@ expected_french = ['Rue', 'Chemin', 'Boulevard', 'Avenue', 'Impasse', 'Concessio
                    'Route', 'Sideroad', 'Montée', 'Promenade', 'Place', 'Voyageur', 
                    'Croissant', 'Principale', 'Parkway', 'Terrace', 'Concourse', ]
 
-
+unexpected = []
 
 ### Helper functions
 
-def print_sorted_dict_alpha(street_types):
-    for s in sorted(street_types.keys(), key=lambda s: s.lower()):
-        print(s, street_types[s])
+# def print_sorted_dict_alpha(street_types_frequency):
+#     for s in sorted(street_types_frequency.keys(), key=lambda s: s.lower()):
+#         print(s, street_types_frequency[s])
 
 
-def print_sorted_dict_frequency(street_types):
-    for w in sorted(street_types, key=street_types.get, reverse=True):
-        print(w, street_types[w])
+# def print_sorted_dict_frequency(street_types_frequency):
+#     for w in sorted(street_types_frequency, key=street_types_frequency.get, reverse=True):
+#         print(w, street_types_frequency[w])
 
 
-def sort_dict_alphabetically(street_types):
-    for s in sorted(street_types.keys(), key=lambda s: s.lower()):
-        yield(s, street_types[s])
+def sort_dict_alphabetically(street_types_frequency):
+    for s in sorted(street_types_frequency.keys(), key=lambda s: s.lower()):
+        yield(s, street_types_frequency[s])
 
 
-def sort_dict_by_frequency(street_types):
-    for s in sorted(street_types, key=street_types.get, reverse=True):
-        yield(s, street_types[s])
+def sort_dict_by_frequency(street_types_frequency):
+    for s in sorted(street_types_frequency, key=street_types_frequency.get, reverse=True):
+        yield(s, street_types_frequency[s])
 
 
 def is_street_name(elem):
     return (elem.attrib['k'] == "addr:street")
 
 
-def audit_street_type(street_types, street_name, expected, street_regex):
+def streets_by_frequency(street_types_frequency, street_name, expected, street_regex):
     m = street_regex.search(street_name)
 
     if m:
         street_type = m.group()
         if str(street_type) in expected:
-            street_types[street_type] += 1
+            street_types_frequency[street_type] += 1
 
 
-### NEW FUNCTIONS
-
-def check_city():
-    pass
-
-    
-def list_region():
-    pass
-
-
-def check_dataset():
-    pass
-
-
-# TODO: This works (I think)! Clean up and construct an equivalent French filter. 
-# Think of what the filter might miss  - will probably need to split
-# street_name and check the first and last entries. UPDATE: This seems to work well, 
-# but try comparing it to output using regex. Also normal string comparisons in Python
+# TODO:  Is regex necessary here? Also normal string comparisons in Python
 # check case - you might need to use write a special check with .lower() or .upper())
-def audit_street_type_filtered(street_types, street_name):
-    
+def filter_street_types(street_types_frequency, street_name):
+    '''
+    Checks if a street belongs to the expected French or English street
+    types. If the street is in neither the street is added to the street_types_frequency
+    list and its frequency is incremented by 1.
+    '''
     split_street_name = street_name.split()
     street_pos_english = street_type_re_english.search(split_street_name[-1])
     street_pos_english = street_pos_english.group()
@@ -105,32 +83,49 @@ def audit_street_type_filtered(street_types, street_name):
     
     if street_pos_english not in expected_english:
         if street_pos_french not in expected_french:
-            street_types[street_pos_english] += 1     
+            street_types_frequency[street_pos_english] += 1     
+
+
+def streets_by_type(street_types_set, street_name, expected, street_regex):
+    m = street_regex.search(street_name)
+    if m:
+        street_type = m.group()
+        if street_type in expected:
+            street_types_set[street_type].add(street_name)
+            
 
 
 ### Main functions
-
-def audit(filename):
-
-    ### ORIGINAL
-    # street_types = defaultdict(set) # this variable show up twice
-
+def audit_by_frequency(filename):
+    '''
+    Returns a list of tuples containing the street type and the number
+    of times the street type appears in the dataset.
+    '''
     for event, elem in ET.iterparse(filename, events=("start",)):
         if elem.tag == "node" or elem.tag == "way":
             for tag in elem.iter("tag"):
                 if is_street_name(tag):
-                    # audit_street_type(street_types, tag.attrib['v'], 
-                    #                     expected_english, street_type_re_english)
-
-                    # audit_street_type(street_types, tag.attrib['v'], 
-                    #                     expected_french, street_type_re_french)
-
-                    audit_street_type_filtered(street_types, tag.attrib['v'])
+                    streets_by_frequency(street_types_frequency, tag.attrib['v'], 
+                                        expected_english, street_type_re_english)
     
-    # pprint.pprint(list(sort_dict_alphabetically(street_types)))
-    # print_sorted_dict_alpha(street_types)
+    return street_types_frequency
 
-    return street_types
+
+# TODO: Output needs to be in dictionary or json format
+def audit_by_type(filename):
+    '''
+    Returns a dictionary of unique street types, with each street type containing
+    a sub-dictionary of the full names of every street of that type.
+    '''
+    for event, elem in ET.iterparse(filename, events=("start",)):
+        if elem.tag == "node" or elem.tag == "way":
+            for tag in elem.iter("tag"):
+                if is_street_name(tag):
+                    streets_by_type(street_types_set, tag.attrib['v'], 
+                                        expected_french, street_type_re_french)
+
+    return street_types_set
+
 
 
 ### Main
@@ -139,7 +134,6 @@ def main():
     import logging
     import os
 
-    
     filename = "ottawa_canada_sample_small.osm" # TODO: Change to relative path before submission
 
     # logging.disable(logging.CRITICAL) # Uncomment to disable logging
@@ -147,18 +141,18 @@ def main():
                         filename=os.path.basename(__file__) + ' - log.txt',level=logging.DEBUG,
                         format=' %(asctime)s - %(levelname)s - %(message)s'
                         )
-    
-    #TODO: resolve logging vs. pprint output
-    # def test_audit():
-        # logging.debug(pprint.pformat(audit(filename)))
-        # logging.debug(pprint.pformat('Special Cases:'))
-        # logging.debug(pprint.pformat('Special Cases:'))
-        # logging.debug(pprint.pformat(audit(filename)))
-    
-    logging.debug(pprint.pformat(list(sort_dict_alphabetically(audit(filename)))))
-    logging.debug(pprint.pformat(list(sort_dict_by_frequency(audit(filename)))))
-    # test_audit()
 
+
+    #TODO: resolve logging vs. pprint output
+    def test_audit():
+        # logging.debug(pprint.pformat('Special Cases:'))
+        logging.debug(pprint.pformat(audit_by_type(filename)))
+        logging.debug(pprint.pformat(audit_by_frequency(filename)))
+        logging.debug(pprint.pformat(list(sort_dict_alphabetically(audit_by_frequency(filename)))))
+        logging.debug(pprint.pformat(list(sort_dict_by_frequency(audit_by_frequency(filename)))))
+    
+    # Run test functions
+    test_audit()
 
 if __name__ == '__main__':
     main()
