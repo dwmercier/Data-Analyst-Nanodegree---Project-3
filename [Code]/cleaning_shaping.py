30e702ebac27bcdcc198e2c5fc98e3d77d6599af
Daniel Mercier <dmercier@gmail.com>
2016-02-26 12:32:38 -0500

    feat: Add new assertion tests for functions

--- [Code]/cleaning_shaping.py
+++ [Code]/cleaning_shaping.py
@@ -28,28 +28,27 @@ import json
 
 # ToDo: add regex patterns for less common street types found in audit
 type_regexes = [
-    [re.compile(r'\sSt\.?$'), 'Street'],
-    [re.compile(r'\s?Blvd\.?\s|\sBlvd\.?$'), 'Boulevard'],
-    [re.compile(r'\s?Ave\.?\s|\sAve\.?$'), 'Avenue'],
-    [re.compile(r'\s?Rd\.?\s|\sRd\.?$'), 'Road'],
-    [re.compile(r'\s?Dr\.?\s|\sDr\.?$'), 'Drive'],
-    [re.compile(r'\s?Ln\.?\s|\sLn\.?$'), 'Lane'],
-    [re.compile(r'\s?Mt\.?\s|\sMt\.?$'), 'Mount'],
-    [re.compile(r'\s?Pkwy\.?\s|\sPkwy\.?$'), 'Parkway']
+    [re.compile(r'\sSt.\s|\sSt\.?$', re.IGNORECASE), 'Street'],
+    [re.compile(r'\s?Blvd\.?\s|\sBlvd\.?$', re.IGNORECASE), 'Boulevard'],
+    [re.compile(r'\s?Ave\.?\s|\sAve\.?$', re.IGNORECASE), 'Avenue'],
+    [re.compile(r'\s?Rd\.?\s|\sRd\.?$', re.IGNORECASE), 'Road'],
+    [re.compile(r'\s?Dr\.?\s|\sDr\.?$', re.IGNORECASE), 'Drive'],
+    [re.compile(r'\s?Ln\.?\s|\sLn\.?$', re.IGNORECASE), 'Lane'],
+    [re.compile(r'\s?Pkwy\.?\s|\sPkwy\.?$', re.IGNORECASE), 'Parkway']
 ]
 
 cardinal_regexes = [
-    [re.compile(r'\s?(?<!\S)S\.?\s|\s(?<!\S)S\.?$'), 'South'],
-    [re.compile(r'\s?(?<!\S)N\.?\s|\s(?<!\S)N\.?$'), 'North'],
-    [re.compile(r'\s?(?<!\S)W\.?\s|\s(?<!\S)W\.?$'), 'West'],
-    [re.compile(r'\s?(?<!\S)E\.?\s|\s(?<!\S)E\.?$'), 'East']
+    [re.compile(r'\s?(?<!\S)S\.?\s|\s(?<!\S)S\.?$', re.IGNORECASE), 'South'],
+    [re.compile(r'\s?(?<!\S)N\.?\s|\s(?<!\S)N\.?$', re.IGNORECASE), 'North'],
+    [re.compile(r'\s?(?<!\S)W\.?\s|\s(?<!\S)W\.?$', re.IGNORECASE), 'West'],
+    [re.compile(r'\s?(?<!\S)E\.?\s|\s(?<!\S)E\.?$', re.IGNORECASE), 'East']
 ]
 
 move_cardinal_regexes = {
-    'East' : re.compile(r'(East[^\w]|East$)'),
-    'West' : re.compile(r'(West[^\w]|West$)'),
-    'South' : re.compile(r'(South$)'),
-    'North' : re.compile(r'(North[^\w]|North$)')
+    'East' : re.compile(r'(East[^\w]|East$)', re.IGNORECASE),
+    'West' : re.compile(r'(West[^\w]|West$)', re.IGNORECASE),
+    'South' : re.compile(r'(South$[^\w]|South$)', re.IGNORECASE),
+    'North' : re.compile(r'(North[^\w]|North$)', re.IGNORECASE)
 }
 
 city_regexes = [
@@ -85,8 +84,6 @@ double_colon = re.compile(r'^([a-z]|_)*:([a-z]|_)*:([a-z]|_)*$') # ToDo: (A) rem
 
 ### Main functions
 
-# ToDo: Decide how to process the 'way' node type tags, <tag k="source" v="CanVec 6.0 - NRCan" /> and 	<tag k="addr:interpolation" v="odd" />
-# ToDo: What to do with 'source' tag?
 def shape_element(element):
     top_level_tags = ['id', 'type', 'visible', 'created', 'address']
     created = [ "version", "changeset", "timestamp", "user", "uid"]
@@ -103,7 +100,7 @@ def shape_element(element):
         for a in attributes: # this refers to the top level elements of the node
             if a in top_level_tags:
                 node[a]= attributes[a]
-
+# ToDo: BUG: pos also gets added to entries that don't originally have a pos; might want to check out all top-level tags
             elif a in pos:
                 if a == 'lon':
                     node['pos'][1] = (float(attributes[a]))
@@ -172,20 +169,20 @@ def check_for_extended_addr(tag_key):
 
 def update_tag(tag_key, tag_value):
     if tag_key[5:] == 'street':
-        tag_value = update_street_name(tag_value)
-        tag_value = fix_cardinal_direction(tag_value)
+        tag_value = update_cardinal_direction(tag_value)
+        tag_value = update_street_type(tag_value)
 
         return tag_value
 
     if tag_key[5:] == 'city':
-        tag_value = compact_city_name(tag_value)
+        tag_value = update_city_name(tag_value)
 
         return tag_value
 
     return tag_value
 
 
-def compact_city_name(tag_value):
+def update_city_name(tag_value):
     for r in city_regexes:
         m = r[0].search(tag_value)
 
@@ -195,38 +192,31 @@ def compact_city_name(tag_value):
     return tag_value
 
 
-def fix_cardinal_direction(tag_value):
+def update_cardinal_direction(tag_value):
     for r in cardinal_regexes:
-        m = r[0].search(tag_value)
+        regex = r[0]
+        replacement = r[1]
+        m = regex.search(tag_value)
 
         if m:
-            tag_value = address_name.replace(str(m.group()), ' ' + r[1])
+            tag_value = tag_value.replace(str(m.group()), ' ' + replacement)
 
-        elif move_cardinal_regexes[r[1]].search(tag_value):
-            tag_value = move_cardinal_direction(tag_value, r[1])
+        if move_cardinal_regexes[replacement].search(tag_value):
+            tag_value = move_cardinal_direction(tag_value, replacement)
 
             return tag_value
 
-        else:
-            return tag_value
+    # else:
+    return tag_value
 
 
-def move_cardinal_direction(tag_value, cardinal_direction):
-
-    # for r in move_cardinal_regexes:
-    #     m = r[0].search(address_name)
-    #
-    #     if m:
-    #         address_name = address_name.replace(str(m.group()), ' ' + r[1])
-    #
-    #         return address_name
-
-    tag_value = cardinal_direction + ' ' + (tag_value.replace(' ' + cardinal_direction, ''))
+def move_cardinal_direction(tag_value, replacement):
+    tag_value = replacement + ' ' + (tag_value.replace(' ' + replacement, ''))
 
     return tag_value
 
 
-def update_street_name(tag_value):
+def update_street_type(tag_value):
     for r in type_regexes:
         m = r[0].search(tag_value)
 
@@ -265,15 +255,15 @@ def capitalize_tag(tag_value):
 
 def clean_tag(tag_key, tag_value):
     tag_value = remove_whitespace(tag_value)
-    tag_value = update_tag(tag_key, tag_value)
     tag_value = capitalize_tag(tag_value)
+    tag_value = update_tag(tag_key, tag_value)
 
     return tag_value
 
 
 def process_map(file_in, pretty = False):
     '''
-    Runs the shape_element and clean functions, then returnswrites json file
+    Runs the shape_element and clean functions, then writes json file
     '''
     file_out = "{0}.json".format(file_in)
     data = []
@@ -290,57 +280,102 @@ def process_map(file_in, pretty = False):
 
                 else:
                     fo.write(json.dumps(el) + "\n")
-# ToDo: The return data is indeed for testing, use it in your assertion tests with the model from the coursework
-    return data
+
 
 
 ### Test functions
-def test():
-    # ToDo: Finish assertion tests
-    # Test shape_element function
-    # assert shape_element(
-    #     <node changeset="6223495" id="969421551" lat="45.3839697" lon="-75.9596713" timestamp="2010-10-30T00:03:00Z" uid="186592" user="Johnwhelan" version="1">
-		# <tag k="source" v="CanVec 6.0 - NRCan" />
-		# <tag k="addr:city" v="City of Ottawa" />
-		# <tag k="addr:street" v="O'hara Drive" />
-		# <tag k="addr:housenumber" v="19" />
-    # </node>
-    # ) ==
+def test_functions():
+    # NOTE: if you are running this code on your computer, with a larger dataset,
+    # call the process_map procedure with pretty=False. The pretty=True option adds
+    # additional spaces to the output, making it significantly larger.
 
-    # Test compact_city_name
-    assert compact_city_name('City of Ottawa') == 'Ottawa'
-    assert compact_city_name('Township Of Tay Valley') == 'Tay Valley'
+    # ToDo: Fix process_map assertion test
 
-    # Test update_element function
+    # Test process_map function
+    # data = process_map('ottawa_canada_sample_tiny.osm', True)
+    # print(dat)
+    # correct_first_elem = {
+    #   "name": "Shell",
+    #   "id": "969421551",
+    #   "created": {
+    #     "user": "Johnwhelan",
+    #     "uid": "186592",
+    #     "version": "1",
+    #     "timestamp": "2010-10-30T00:03:00Z",
+    #     "changeset": "6223495"
+    #   },
+    #   "address": {
+    #     "housenumber": "19",
+    #     "city": "Ottawa",
+    #     "street": "O'hara Drive"
+    #   },
+    #   "website": "http://www.shell.ca/",
+    #   "source": "CanVec 6.0 - NRCan",
+    #   "amenity": "fuel",
+    #   "type": "node",
+    #   "pos": [
+    #     45.3839697,
+    #     -75.9596713
+    #   ]
+    # }
+    #
+    # assert data[0] == correct_first_elem
 
-    # Test fix_cardinal_direction function
+
+    # Test check_for_extended_addr function
+    assert check_for_extended_addr('addr::') == True
+    assert check_for_extended_addr('addr:') == False
+
+    # Test update_city_name function
+    assert update_city_name('City of Ottawa') == 'Ottawa'
+    assert update_city_name('Township Of Tay Valley') == 'Tay Valley'
+
+    # Test update_cardinal_direction function
+    assert update_cardinal_direction('Baker Street S.') == 'South Baker Street'
+    assert update_cardinal_direction('Baker Street E.') == 'East Baker Street'
+    assert update_cardinal_direction('Baker Street N') == 'North Baker Street'
+    assert update_cardinal_direction('Baker Street W') == 'West Baker Street'
 
     # Test move_cardinal_direction function
+    assert move_cardinal_direction('Baker Street South', 'South') == 'South Baker Street'
+    assert move_cardinal_direction('Baker Street East', 'East') == 'East Baker Street'
+    assert move_cardinal_direction('Baker Street North', 'North') == 'North Baker Street'
+    assert move_cardinal_direction('Baker Street West', 'West') == 'West Baker Street'
 
     # Test remove_whitespace function
     assert remove_whitespace('Baker  Street') == 'Baker Street'
     assert remove_whitespace('Baker   Street') == 'Baker Street'
     assert remove_whitespace(' Baker   Street ') == 'Baker Street'
 
-    # Test update_street_name function
+    # Test update_street_type function
+    assert update_street_type('South Ash St.') == 'South Ash Street'
+    assert update_street_type('St. Ash St.') == 'St. Ash Street'
+    assert update_street_type('South Ash Rd.') == 'South Ash Road'
+    assert update_street_type('South Ash Dr.') == 'South Ash Drive'
+    assert update_street_type('South Ash Ave.') == 'South Ash Avenue'
+    assert update_street_type('South Ash Ln') == 'South Ash Lane'
+    assert update_street_type('South Ash Pkwy') == 'South Ash Parkway'
 
     # Test capitalize_tag function
-    # assert capitalize_tag('baker Street') == 'Baker Street'
-    # assert capitalize_tag('south baker street') == 'South Baker Street'
+    assert capitalize_tag('baker Street') == 'Baker Street'
+    assert capitalize_tag('south baker street') == 'South Baker Street'
 
-    # Test clean function
-    # assert clean_tag('addr:street', 'Chanonhouse St.') == 'Chanonhouse Street'
-    # assert clean_tag('addr:street', 'St. Jerome St.') == 'St. Jerome Street'
-    # assert clean_tag('addr:street', 'South Ash Ln ') == 'South Ash Lane'
-    # assert clean_tag('addr:street', 'Baker Street South') == 'South Baker Street'
-    pass
+    # Test update_tag function function
+    assert update_tag('addr:city', 'City of Ottawa') == 'Ottawa'
+    assert update_tag('addr:street', 'Ash St. S.') == 'South Ash Street'
+
+    # Test clean_tag function
+    assert clean_tag('addr:street', 'Chanonhouse st.') == 'Chanonhouse Street'
+    assert clean_tag('addr:street', 'St. Jerome St') == 'St. Jerome Street'
+    assert clean_tag('addr:street', 'South Ash Ln ') == 'South Ash Lane'
+    assert clean_tag('addr:street', 'Baker   Street south') == 'South Baker Street'
 
 def main():
-    test()
     filename = "ottawa_canada_sample_tiny.osm"
 
-    process_map(filename, True)
+    test_functions()
 
+    process_map(filename, True)
 
 
 if __name__ == '__main__':
